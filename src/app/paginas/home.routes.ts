74d9ec729e3e27635d8ea7035a6bc2db7c64a0e1
Garieli <gabicristina015@gmail.com>
2024-05-02 20:27:42 -0300

    coomit

--- src/app/paginas/home.routes.ts
+++ src/app/paginas/home.routes.ts
@@ -12,6 +12,9 @@ export const routes: Routes = [
       { path: 'cadastro/veiculo/adicionar', loadChildren: () => import('./cadastros/veiculo/veiculo-formulario/veiculo-formulario.module').then(m => m.VeiculoFormularioModule), data: { breadcrumb: 'Adicionar' } },
       { path: 'cadastro/pacote', loadChildren: () => import('./cadastros/pacote/pacote.module').then(m => m.CadastroPacoteModule), data: { breadcrumb: 'Pacote' } },
       { path: 'cadastro/pacote/adicionar', loadChildren: () => import('./cadastros/pacote/pacote-formulario/pacote-formulario.module').then(m => m.PacoteFormularioModule), data: { breadcrumb: 'Adicionar' } },
+      { path: 'cadastro/viagem', loadChildren: () => import('./cadastros/viagem/viagem.module').then(m => m.CadastroViagemModule), data: { breadcrumb: 'Viagem' } },
+      { path: 'cadastro/viagem/adicionar', loadChildren: () => import('./cadastros/viagem/viagem-formulario/viagem-formulario.module').then(m => m.ViagemFormularioModule), data: { breadcrumb: 'Adicionar' } },
+
     ]
   },
 ];
