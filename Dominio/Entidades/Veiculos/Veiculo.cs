756d97be5c5f807760743cad3125cd58a6d3ee56
gustavosilvani <gustavosilvani@hotmail.com>
2024-08-15 00:08:26 -0300

    Update Veiculo.cs

--- Dominio/Entidades/Veiculos/Veiculo.cs
+++ Dominio/Entidades/Veiculos/Veiculo.cs
@@ -4,18 +4,18 @@ namespace Dominio.Entidades.Veiculos
 {
     public class Veiculo : BaseEntity
     {
-        public string Placa { get; private set; }
-        public string Modelo { get; private set; }
-        public string Marca { get; private set; }
-        public int Ano { get; private set; }
-        public string Cor { get; private set; }
-        public string Renavam { get; private set; }
-        public TipoCombustivelEnum TipoCombustivel { get; private set; }
-        public TipoVeiculoEnum TipoVeiculo { get; private set; }
-        public string Capacidade { get; private set; }
-        public CategoriaCNHEnum CategoriaCNH { get; private set; }
-        public StatusVeiculoEnum Status { get; private set; }
-        public string Observacao { get; private set; }
+        public string Placa { get; }
+        public string Modelo { get; }
+        public string Marca { get; }
+        public int Ano { get; }
+        public string Cor { get; }
+        public string Renavam { get; }
+        public TipoCombustivelEnum TipoCombustivel { get; }
+        public TipoVeiculoEnum TipoVeiculo { get; }
+        public string Capacidade { get; }
+        public CategoriaCNHEnum CategoriaCNH { get; }
+        public StatusVeiculoEnum Status { get; }
+        public string Observacao { get; }
         protected override string Descricao => Id.ToString();
     }
 }
