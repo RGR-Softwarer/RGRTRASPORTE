cb00ba67b6b5f561573961d256280cf37148910b
gustavosilvani <gustavosilvani@hotmail.com>
2024-08-21 23:26:35 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://localhost:5200/api',
-    apiTrasportador: 'http://localhost:5200/api/',
+    apiBaseUrl: 'http://66.135.11.124:4000/api',
+    apiTrasportador: 'http://66.135.11.124:4000/api/',
     authToken: 'Bearer YYC2U1DuZfaZOiQAPsZkh6dWLGeFZgxLSYHqaO5Dj1o='
   };
   
