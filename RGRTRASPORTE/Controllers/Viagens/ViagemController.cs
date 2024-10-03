a3ba4d7b856c8c370f1b96763ad23f306b4a287b
gustavosilvani <gustavosilvani@hotmail.com>
2024-10-03 20:37:21 -0300

    ajuste rota

--- RGRTRASPORTE/Controllers/Viagens/ViagemController.cs
+++ RGRTRASPORTE/Controllers/Viagens/ViagemController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Dominio.Dtos.Viagens;
+using Dominio.Entidades.Viagens;
 using Dominio.Interfaces.Infra.Data;
 using Dominio.Interfaces.Service.Viagens;
 using Infra.CrossCutting.Handlers.Notifications;
@@ -34,6 +35,48 @@ namespace RGRTRASPORTE.Controllers.Viagens
                 return NoContent();
 
             return await RGRResult(System.Net.HttpStatusCode.OK, viagem);
+        }
+
+        [HttpGet("ObterRotaViagem/{id}")]
+        public async Task<IActionResult> ObterRotaViagem(long id)
+        {
+            var latirudeOrigem = "-27.100"; // Latitude de Chapecó
+            var logidetudaOrigem = "-52.615"; // Longitude de Chapecó
+            var latirudeDestino = "-26.950"; // Latitude de Xaxim
+            var logidetudaDestino = "-52.537"; // Longitude de Xaxim
+            var latirudeParada = "-27.203"; // Latitude de Cordilheira Alta (parada)
+            var logidetudaParada = "-52.605"; // Longitude de Cordilheira Alta (parada)
+            var apiKey = "AIzaSyA5KlWJ_Thsl-U6FN2TnLU8TvvHhCyPgwo"; // Substitua pela sua API Key
+
+            // Construindo a URL com parada (waypoint) em Cordilheira Alta
+            var url = $"https://maps.googleapis.com/maps/api/directions/json?origin={latirudeOrigem},{logidetudaOrigem}&destination={latirudeDestino},{logidetudaDestino}&waypoints={latirudeParada},{logidetudaParada}&key={apiKey}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Fazendo a requisição GET
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    // Verificando se a requisição foi bem-sucedida
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Lendo o conteúdo da resposta
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        return await RGRResult(System.Net.HttpStatusCode.OK, responseBody);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Erro: {response.StatusCode}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Exceção: {e.Message}");
+                }
+            }       
+
+            return await RGRResult(System.Net.HttpStatusCode.BadRequest, "Erro ao obter a rota da viagem");
         }
 
         [HttpPost]
