5460ad2100159aad14b80640decbccc9149b5c45
gustavosilvani <gustavosilvani@hotmail.com>
2024-11-13 20:06:17 -0300

    Update ModeloVeicularDto.cs

--- Dominio/Dtos/Veiculo/ModeloVeicularDto.cs
+++ Dominio/Dtos/Veiculo/ModeloVeicularDto.cs
@@ -6,7 +6,7 @@ namespace Dominio.Dtos.Veiculo
     {
         public long Id { get; set; }
         public bool Situacao { get; init; }
-        public bool DescricaoModelo { get; init; }
+        public string DescricaoModelo { get; init; }
         public TipoModeloVeiculoEnum Tipo { get; init; }
         public int QuantidadeAssento { get; init; }
         public int QuantidadeEixo { get; init; }
